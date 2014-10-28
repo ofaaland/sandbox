746518a6a3f2d8b94ecf90acffcdebb1f038c395
Olaf Faaland <faaland1@llnl.gov>
2014-10-28 16:05:14 -0700

    testing simpler /etc/gitconfig

--- temp.c
+++ temp.c
@@ -7,4 +7,5 @@ int main(int argc, char *argv)
 
 	printf("welcome to branch new_test_push\n");
 	printf("now with non-xwindows password prompts!\n");
+	printf("now with proxy listed only in section [http] of /etc/gitconfig!\n");
 }
